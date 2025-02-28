a74524d2da4e1176b33fee70e7cf5c1d2a5a49f4
Guilherme Oliveira Casagrande <guigacasa@protonmail.ch>
2025-02-27 22:15:43 -0300

    added login placeholder

--- app/account/login.tsx
+++ app/account/login.tsx
@@ -1,12 +1,16 @@
 import ButtonLink from "@/components/ButtonLink";
 
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, ImageBackground } from "react-native";
+
+const placholderScreenPath = require("@/assets/images/placeholder/LOGIN.png");
 
 export default function AccountLoginScreen() {
   return (
-    <View>
-      <Text style={styles.bigText}>UPDATING</Text>
-      <ButtonLink text="HOME" href="/home/" />
+    <View style={styles.container}>
+      <ImageBackground style={styles.bg} source={placholderScreenPath}>
+        <Text style={styles.bigText}>UPDATING</Text>
+        <ButtonLink text="HOME" href="/home/" />
+      </ImageBackground>
     </View>
   );
 }
@@ -14,5 +18,13 @@ export default function AccountLoginScreen() {
 const styles = StyleSheet.create({
   bigText: {
     fontSize: 20,
+    color: "#fff",
+  },
+  bg: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    padding: 20,
   },
 });
