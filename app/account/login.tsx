fd70405f5f20d7c31c05131d3cbd991b889f3293
Guilherme Oliveira Casagrande <guigacasa@protonmail.ch>
2025-03-14 11:38:17 -0300

    added base components for account login screen

--- app/account/login.tsx
+++ app/account/login.tsx
@@ -1,30 +1,41 @@
-import ButtonLink from "@/components/ButtonLink";
+import { View, StyleSheet, Text } from "react-native";
 
-import { Text, View, StyleSheet, ImageBackground } from "react-native";
-
-const placholderScreenPath = require("@/assets/images/placeholder/LOGIN.png");
+import Input from "@/components/Input";
+import BubbleIcon from "@/components/BubbleIcon";
+import Footer from "@/components/Footer";
+import ButtonSumbit from "@/components/ButtonSubmit";
 
 export default function AccountLoginScreen() {
   return (
-    <View style={styles.container}>
-      <ImageBackground style={styles.bg} source={placholderScreenPath}>
-        <Text style={styles.bigText}>LOGIN</Text>
-        <ButtonLink text="HOME" href="/home/" />
-      </ImageBackground>
+    <View style={styles.viewContainer}>
+      <Text style={styles.text}>Português (Brasil)</Text>
+      <BubbleIcon width={300} height={300} />
+      <Input label="Nome de usuário" placeholder="@exemplo" />
+      <Input
+        label="Senha"
+        alterLink="Esqueceu a senha?"
+        placeholder="***********"
+      />
+      <ButtonSumbit title="Entrar" />
+      <Footer title="Não tem uma conta? Cadastre-se" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  bigText: {
+  text: {
+    color: "white",
     fontSize: 20,
-    color: "#fff",
   },
-  bg: {
+  viewContainer: {
+    paddingTop: 20,
     flex: 1,
-  },
-  container: {
-    flex: 1,
-    padding: 20,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    gap: "8%",
+    backgroundColor: "#200243",
+    borderColor: "coral",
+    borderWidth: 1,
   },
 });
